b60a7670eeb6869312a0557a85921ae2a2638014
Benjamin Dornel <benjamindornel@gmail.com>
2023-08-06 18:42:45 +0800

    feat(tf): add bucket for processed files

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,12 @@ resource "google_service_account" "service_account" {
   display_name = "Monopoly"
 }
 
-module "gmail" {
-  source = "./modules/gmail"
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "monopoly-${random_id.bucket_prefix.hex}"
+  location      = "US"
+  storage_class = "STANDARD"
 }
