2366432ec4df16698f06c093d8f61ee176518dc7
Benjamin Dornel <benjamindornel@gmail.com>
2023-08-06 17:18:33 +0800

    feat(ocbc): add test for 365

---
+++ tests/test_pdf.py
@@ -0,0 +1,18 @@
+from monopoly.banks.ocbc import OCBC
+
+
+def test_ocbc_unprotected_pdf():
+    pdf = OCBC(file_path="tests/ocbc_365.pdf")
+    pdf.extract()
+
+    assert pdf.df.columns.values.tolist() == ["Date", "Description", "Amount"]
+    assert pdf.df.loc[0].to_dict() == {
+        "Date": "12/06",
+        "Description": " FAIRPRICE FINEST SINGAPORE SG ",
+        "Amount": "18.49"
+    }
+    assert pdf.df.loc[1].to_dict() == {
+        "Date": "12/06",
+        "Description": " DA PAOLO GASTRONOMIA SING — SINGAPORE SG ",
+        "Amount": "19.69"
+    }
