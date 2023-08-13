9ac51e33b1090a9dcbff6335c4f12e9fa179ed78
Benjamin Dornel <benjamindornel@gmail.com>
2023-08-13 16:03:07 +0800

    test(ocbc/transform): add test case for data within year

--- tests/test_ocbc_transform.py
+++ tests/test_ocbc_transform.py
@@ -7,7 +7,7 @@ from monopoly.banks.ocbc import OCBC
 from monopoly.constants import AMOUNT, DATE, DESCRIPTION
 
 
-def test_ocbc_transform():
+def test_ocbc_transform_cross_year():
     ocbc = OCBC(file_path="tests/ocbc_365.pdf")
     ocbc.statement_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
 
@@ -37,6 +37,45 @@ def test_ocbc_transform():
             },
             {
                 DATE: "2023-12-28",
+                DESCRIPTION: "DA PAOLO GASTRONOMIA SING — SINGAPORE SG",
+                AMOUNT: 19.69,
+            },
+        ]
+    )
+
+    assert_frame_equal(transformed_df, expected_data)
+
+
+def test_ocbc_transform_within_year():
+    ocbc = OCBC(file_path="tests/ocbc_365.pdf")
+    ocbc.statement_date = datetime.strptime("2023-07-01", "%Y-%m-%d")
+
+    ocbc.df = pd.DataFrame(
+        [
+            {
+                DATE: "12/06",
+                DESCRIPTION: "FAIRPRICE FINEST SINGAPORE SG",
+                AMOUNT: "18.49",
+            },
+            {
+                DATE: "12/06",
+                DESCRIPTION: "DA PAOLO GASTRONOMIA SING — SINGAPORE SG",
+                AMOUNT: "19.69",
+            },
+        ]
+    )
+
+    transformed_df = ocbc.transform()
+
+    expected_data = pd.DataFrame(
+        [
+            {
+                DATE: "2023-06-12",
+                DESCRIPTION: "FAIRPRICE FINEST SINGAPORE SG",
+                AMOUNT: 18.49,
+            },
+            {
+                DATE: "2023-06-12",
                 DESCRIPTION: "DA PAOLO GASTRONOMIA SING — SINGAPORE SG",
                 AMOUNT: 19.69,
             },
