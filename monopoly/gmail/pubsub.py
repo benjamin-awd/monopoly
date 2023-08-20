7161fd86ca5a28046b1f5e6b5dc672b1abb44a4f
Benjamin Dornel <{{ .email }}>
2023-08-20 22:26:35 +0800

    refactor: use pydantic for env var management

--- monopoly/gmail/pubsub.py
+++ monopoly/gmail/pubsub.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 import json
 import logging
-import os
 from typing import TYPE_CHECKING
 
 from google.cloud import secretmanager
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
+
+from monopoly.config import settings
 
 if TYPE_CHECKING:
     from googleapiclient._apis.gmail.v1.resources import GmailResource
@@ -15,17 +16,15 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-def get_credentials(secret_id="monopoly-gmail-token", version_id="latest"):
+def get_credentials(secret_id=settings.secret_id, version_id="latest"):
     scopes = [
         "https://www.googleapis.com/auth/gmail.readonly",
         "https://www.googleapis.com/auth/gmail.settings.basic",
         "https://www.googleapis.com/auth/gmail.labels",
     ]
 
-    project_id = os.environ.get("PROJECT_ID")
-
     client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    name = f"projects/{settings.project_id}/secrets/{secret_id}/versions/{version_id}"
     response = client.access_secret_version(name=name)
 
     credentials = Credentials.from_authorized_user_info(
@@ -47,19 +46,15 @@ def get_gmail_service() -> GmailResource:
 
 
 def set_up_gmail_push_notifications():
-    pubsub_topic = os.environ.get("PUBSUB_TOPIC")
-    project_id = os.environ.get("PROJECT_ID")
-    gmail_address = os.environ.get("GMAIL_ADDRESS")
-
     service = get_gmail_service()
     request_body = {
         "labelIds": ["INBOX"],
-        "topicName": f"projects/{project_id}/topics/{pubsub_topic}",
+        "topicName": f"projects/{settings.project_id}/topics/{settings.pubsub_topic}",
     }
 
     return (
         service.users()
-        .watch(userId=gmail_address, body=request_body)
+        .watch(userId=settings.gmail_address, body=request_body)
         .execute()
     )
 
