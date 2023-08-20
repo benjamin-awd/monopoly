00240eb2dac8df4ffb1a0141cc706d555e494a5d
Benjamin Dornel <{{ .email }}>
2023-08-20 16:50:00 +0800

    feat(tf): add pubsub topic and give access to gmail

---
+++ terraform/pubsub.tf
@@ -0,0 +1,18 @@
+resource "google_pubsub_topic" "default" {
+  name    = "monopoly-topic"
+  project = "phonic-ceremony-394407"
+
+}
+
+module "pubsub_topic-iam-bindings" {
+  source        = "terraform-google-modules/iam/google//modules/pubsub_topics_iam"
+  project       = "phonic-ceremony-394407"
+  pubsub_topics = [google_pubsub_topic.default.id]
+  mode          = "authoritative"
+
+  bindings = {
+    "roles/pubsub.publisher" = [
+      "serviceAccount:gmail-api-push@system.gserviceaccount.com",
+    ]
+  }
+}
