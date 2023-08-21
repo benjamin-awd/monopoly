fb816b7ef85b94edc7a70d93c057d4a3edcc5200
Benjamin Dornel <{{ .email }}>
2023-08-21 23:34:00 +0800

    refactor(gmail): move credential functions to separate file

--- monopoly/gmail/pubsub.py
+++ monopoly/gmail/pubsub.py
@@ -1,14 +1,10 @@
 from __future__ import annotations
 
-import json
 import logging
 from typing import TYPE_CHECKING
 
-from google.cloud import secretmanager
-from google.oauth2.credentials import Credentials
-from googleapiclient.discovery import build
-
 from monopoly.config import settings
+from monopoly.gmail.credentials import get_gmail_service
 
 if TYPE_CHECKING:
     from googleapiclient._apis.gmail.v1.resources import GmailResource
@@ -16,42 +12,14 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-def get_credentials(secret_id=settings.secret_id, version_id="latest"):
-    scopes = [
-        "https://www.googleapis.com/auth/gmail.readonly",
-        "https://www.googleapis.com/auth/gmail.settings.basic",
-        "https://www.googleapis.com/auth/gmail.labels",
-    ]
-
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{settings.project_id}/secrets/{secret_id}/versions/{version_id}"
-    response = client.access_secret_version(name=name)
-
-    credentials = Credentials.from_authorized_user_info(
-        info=json.loads(response.payload.data), scopes=scopes
-    )
-    return credentials
-
-
-def get_gmail_service() -> GmailResource:
-    credentials = get_credentials()
-
-    try:
-        service = build("gmail", "v1", credentials=credentials)
-        return service
-
-    except Exception as err:
-        logger.error(err)
-        raise err
-
-
 def set_up_gmail_push_notifications():
-    service = get_gmail_service()
+    service: GmailResource = get_gmail_service()
     request_body = {
         "labelIds": ["INBOX"],
         "topicName": f"projects/{settings.project_id}/topics/{settings.pubsub_topic}",
     }
 
+    # pylint: disable=E1101
     return (
         service.users()
         .watch(userId=settings.gmail_address, body=request_body)
