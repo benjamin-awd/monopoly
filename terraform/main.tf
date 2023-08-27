c5d1d5db53a0a25c20ee283e477b391da8867e8a
Benjamin Dornel <{{ .email }}>
2023-08-27 16:51:21 +0800

    feat(tf): create iac for cloud run function

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,48 @@ resource "google_storage_bucket_iam_member" "default" {
   role   = "roles/storage.admin"
   member = "serviceAccount:${google_service_account.default.email}"
 }
+
+resource "google_cloud_run_v2_job" "default" {
+  name     = "monopoly-tf"
+  location = local.region
+
+  template {
+    template {
+      service_account = google_service_account.default.email
+      max_retries     = 1
+
+      containers {
+        image = "${local.region}-docker.pkg.dev/${local.project_id}/monopoly/monopoly:main"
+
+        env {
+          name  = "PUBSUB_TOPIC"
+          value = google_pubsub_topic.default.id
+        }
+        env {
+          name  = "GMAIL_ADDRESS"
+          value = var.gmail_address
+        }
+        env {
+          name  = "PROJECT_ID"
+          value = local.project_id
+        }
+        env {
+          name  = "GCS_BUCKET"
+          value = google_storage_bucket.default.name
+        }
+        env {
+          name  = "SECRET_ID"
+          value = local.gmail_credential_secret
+        }
+        env {
+          name  = "TRUSTED_USER_EMAILS"
+          value = jsonencode(var.trusted_emails)
+        }
+        env {
+          name  = "OCBC_PDF_PASSWORD"
+          value = var.ocbc_password
+        }
+      }
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "ocbc_password" {
+  description = "Password for encrypted OCBC PDFs"
+  type        = string
+  sensitive   = true
+}
+
+variable "gmail_address" {
+  description = "Gmail address to which bank statements/transactions are sent"
+  type        = string
+  sensitive   = true
+}
+
+variable "trusted_emails" {
+  description = "Trusted user emails"
+  type        = list
+  sensitive   = true
+}
