5d412be3dc2bf714c8fcad18b24a635a5e9005a1
Benjamin Dornel <benjamindornel@gmail.com>
2023-08-27 17:16:47 +0800

    refactor(pdf): change _open_pdf to public static method

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -4,15 +4,15 @@ from monopoly.pdf import PDF
 
 
 def test_can_open_protected(pdf: PDF):
-    pdf.file_path = "tests/ocbc_365_protected.pdf"
-    pdf.password = "foobar123"
+    file_path = "tests/ocbc_365_protected.pdf"
+    password = "foobar123"
 
-    pdf._open_pdf()
+    pdf.open(file_path, password)
 
 
 def test_wrong_password_raises_error(pdf: PDF):
-    pdf.file_path = "tests/ocbc_365_protected.pdf"
-    pdf.password = "wrong_pw"
+    file_path = "tests/ocbc_365_protected.pdf"
+    password = "wrong_pw"
 
     with pytest.raises(ValueError, match="document is encrypted"):
-        pdf._open_pdf()
+        pdf.open(file_path, password)
