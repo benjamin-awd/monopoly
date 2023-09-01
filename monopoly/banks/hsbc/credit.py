2c808104999bf7e0fed4f621be300bb68c7ae971
Benjamin Dornel <{{ .email }}>
2023-09-01 23:55:06 +0800

    refactor(banks): DRY cross-year date transformation logic

--- monopoly/banks/hsbc/credit.py
+++ monopoly/banks/hsbc/credit.py
@@ -21,7 +21,7 @@ class HsbcRevolution(Bank):
     )
 
     def __init__(self, pdf_file_path: str):
-        date_converter = self.convert_date
+        date_parser = self.get_date_parts
         super().__init__(
             account_name="Revolution",
             bank_name="HSBC",
@@ -33,19 +33,11 @@ class HsbcRevolution(Bank):
                 page_range=(0, -1),
                 page_bbox=(0, 0, 379, 842),
             ),
-            date_converter=date_converter,
+            date_parser=date_parser,
         )
 
-    def convert_date(self, row):
-        logger.info("Transforming dates to ISO 8601")
+    def get_date_parts(self, row):
         split = row[DATE].split(" ")
         row_day = int(split[0])
         row_month = datetime.strptime(split[1], "%b").month
-
-        # Deal with mixed years from Jan/Dec
-        if self.statement.statement_date.month == 1 and row_month == 12:
-            row_year = self.statement.statement_date.year - 1
-        else:
-            row_year = self.statement.statement_date.year
-
-        return f"{row_year}-{row_month:02d}-{row_day:02d}"
+        return row_day, row_month
--- monopoly/banks/ocbc/credit.py
+++ monopoly/banks/ocbc/credit.py
@@ -16,25 +16,16 @@ class Ocbc365(Bank):
     )
 
     def __init__(self, pdf_file_path: str):
-        date_converter = self.convert_date
+        date_parser = self.get_date_parts
         super().__init__(
             account_name="365",
             bank_name="OCBC",
             statement=Statement(**self.statement_config.__dict__),
             transform_dates=True,
             pdf=Pdf(pdf_file_path, settings.ocbc_pdf_password, (0, -2)),
-            date_converter=date_converter,
+            date_parser=date_parser,
         )
 
-    def convert_date(self, row):
-        logger.info("Transforming dates to ISO 8601")
-
+    def get_date_parts(self, row):
         row_day, row_month = map(int, row[DATE].split("/"))
-
-        # Deal with mixed years from Jan/Dec
-        if self.statement.statement_date.month == 1 and row_month == 12:
-            row_year = self.statement.statement_date.year - 1
-        else:
-            row_year = self.statement.statement_date.year
-
-        return f"{row_year}-{row_month:02d}-{row_day:02d}"
+        return row_day, row_month
