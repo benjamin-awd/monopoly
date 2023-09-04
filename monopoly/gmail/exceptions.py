1d40d5d00e56f44a3a4043400a4143a649dd051c
Benjamin Dornel <benjamindornel@gmail.com>
2023-09-04 22:15:12 +0800

    feat(gmail): add support for nested parts

--- monopoly/gmail/exceptions.py
+++ monopoly/gmail/exceptions.py
@@ -4,3 +4,7 @@ class MultipleAttachmentsError(Exception):
 
 class UntrustedUserError(Exception):
     """Raise when email received from an untrusted user"""
+
+
+class AttachmentNotFoundError(Exception):
+    """Raise when attachment not found in email"""
