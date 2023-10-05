e560d8066584fafa88cb0a3100c5fe296e3b0a34
Benjamin Dornel <benjamindornel@gmail.com>
2023-10-05 23:37:36 +0800

    feat(hsbc): allow password to override masking

--- monopoly/banks/hsbc.py
+++ monopoly/banks/hsbc.py
@@ -24,8 +24,8 @@ class Hsbc(BankBase):
     )
 
     pdf_config = PdfConfig(
-        password=settings.hsbc_pdf_password_prefix,
+        static_string=settings.hsbc_pdf_password_prefix,
+        brute_force_mask="?d?d?d?d?d?d",
         page_range=(0, -1),
         page_bbox=(0, 0, 379, 842),
-        brute_force_mask="?d?d?d?d?d?d",
     )
