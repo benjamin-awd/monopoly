afd1eaca81958c2cac4ef15fc9c7b1f0b72500c0
Benjamin Dornel <benjamindornel@gmail.com>
2023-10-07 00:30:53 +0800

    refactor(citibank): add page bbox and only match from string start

--- monopoly/banks/citibank.py
+++ monopoly/banks/citibank.py
@@ -13,11 +13,15 @@ class Citibank(BankBase):
         bank_name=BankNames.CITIBANK,
         account_type=AccountType.CREDIT,
         transaction_pattern=(
-            r"(?P<date>\b\d{2}\s\w{3}\b)\s*(?P<description>.*?)\s*(?P<amount>[\d.,]+)$"
+            r"^(?P<date>\b\d{2}\s\w{3}\b)\s*(?P<description>.*?)\s*(?P<amount>[\d.,]+)$"
         ),
         transaction_date_format="%d %b",
         date_pattern=r"Statement\sDate\s(.*)$",
         statement_date_format=r"%B %d, %Y",
     )
 
-    pdf_config = PdfConfig(password=settings.citibank_pdf_password, page_range=(0, -3))
+    pdf_config = PdfConfig(
+        password=settings.citibank_pdf_password,
+        page_bbox=(20, 0, 595, 840),
+        page_range=(0, -3),
+    )
