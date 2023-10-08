4d825448e582954fba2b6348d83adf366657dbb4
Benjamin Dornel <benjamindornel@gmail.com>
2023-10-08 15:52:16 +0800

    refactor(examples): use ficitional bank example

--- monopoly/examples/single_statement.py
+++ monopoly/examples/single_statement.py
@@ -1,12 +1,31 @@
-from monopoly.banks.ocbc import Ocbc
+from monopoly.bank import BankBase
+from monopoly.helpers.constants import AccountType, BankNames
+from monopoly.statement import StatementConfig
 
 
-def ocbc_example():
+# fmt: off
+class MonopolyBank(BankBase):
+    """Dummy class to help with reading the example PDF statement"""
+    statement_config = StatementConfig(
+        bank_name=BankNames.EXAMPLE,
+        account_type=AccountType.CREDIT,
+        transaction_pattern=(
+            r"(?P<date>\d+/\d+)\s*"
+            r"(?P<description>.*?)\s*"
+            r"(?P<amount>[\d.,]+)$"
+        ),
+        transaction_date_format=r"%d/%m",
+        statement_date_pattern=r"\d{2}\-\d{2}\-\d{4}",
+        statement_date_format=r"%d-%m-%Y",
+    )
+
+
+def example():
     """Example showing how monopoly can be used to extract data from
     a single bank statement
     """
-    bank = Ocbc(
-        file_path="monopoly/examples/ocbc_365_example.pdf",
+    bank = MonopolyBank(
+        file_path="monopoly/examples/example_statement.pdf",
     )
 
     # This runs Tesseract on the PDF and
@@ -18,9 +37,9 @@ def ocbc_example():
     print(transformed_df)
 
     # Files are saved to an output directory
-    # monopoly/output/OCBC-Credit-2023-08.csv
+    # monopoly/output/monopoly-credit-2023-08.csv
     bank.load(transformed_df, statement)
 
 
 if __name__ == "__main__":
-    ocbc_example()
+    example()
