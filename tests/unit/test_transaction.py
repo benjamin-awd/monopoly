87d426f9bd867771dfbc408f5d8963abe5fcfaae
Benjamin Dornel <benjamindornel@gmail.com>
2023-10-31 23:35:49 +0800

    fix(ci): update local hook to use task for test

--- tests/unit/test_transaction.py
+++ tests/unit/test_transaction.py
@@ -4,3 +4,8 @@ from monopoly.statement import Transaction
 def test_transaction_handles_comma():
     transaction = Transaction("2099-09-10", "foo", "123,123.12")
     assert transaction.amount == 123123.12
+
+
+def test_transaction_handles_whitespace():
+    transaction = Transaction("2099-09-10", "   foo,     bar    ", "123,123.12")
+    assert transaction.description == "foo, bar"
