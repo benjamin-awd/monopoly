c8741b789fe7b672911a66fcc76862a3fcc21d7c
Benjamin Dornel <benjamindornel@gmail.com>
2023-11-22 15:33:24 +0000

    refactor(statement): treat credit card transactions as debit entry

--- tests/unit/test_transaction.py
+++ tests/unit/test_transaction.py
@@ -3,7 +3,7 @@ from monopoly.statement import Transaction
 
 def test_transaction_handles_comma():
     transaction = Transaction("2099-09-10", "foo", "123,123.12")
-    assert transaction.amount == 123123.12
+    assert transaction.amount == -123123.12
 
 
 def test_transaction_handles_whitespace():
