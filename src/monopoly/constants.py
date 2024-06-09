ac2748b4c83a318cca441ee88e8c718e73321c68
Benjamin Dornel <benjamindornel@gmail.com>
2024-06-09 23:45:19 +0800

    chore(generic/constants): change all patterns to raw string

--- src/monopoly/constants.py
+++ src/monopoly/constants.py
@@ -8,12 +8,12 @@ from pydantic.dataclasses import dataclass
 # flake8: noqa
 # pylint: disable=line-too-long
 class DateFormats(StrEnum):
-    D = "(?:1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)"
-    DD = "(?:01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)"
-    M = "(?:1|2|3|4|5|6|7|8|9|10|11|12)"
-    MM = "(?:01|02|03|04|05|06|07|08|09|10|11|12)"
-    MMM = "(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)"
-    MMMM = "(?:January|February|March|April|May|June|July|August|September|October|November|December)"
+    D = r"(?:1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)"
+    DD = r"(?:01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)"
+    M = r"(?:1|2|3|4|5|6|7|8|9|10|11|12)"
+    MM = r"(?:01|02|03|04|05|06|07|08|09|10|11|12)"
+    MMM = r"(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)"
+    MMMM = r"(?:January|February|March|April|May|June|July|August|September|October|November|December)"
     YY = r"(?:[2-5][0-9])"
     YYYY = r"(?:20\d{2})"
 
