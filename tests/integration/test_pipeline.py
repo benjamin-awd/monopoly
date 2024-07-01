b5b641c28bd20b9376dc1c2cf279a9d45470bbef
Benjamin Dornel <benjamindornel@gmail.com>
2024-07-01 23:11:04 +0800

    test: add tests for pipeline with bank arg

--- tests/integration/test_pipeline.py
+++ tests/integration/test_pipeline.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pytest
 
+from monopoly.banks import Dbs, ExampleBank
 from monopoly.pipeline import Pipeline
 
 
@@ -18,6 +19,21 @@ def test_pipeline_initialization_with_bytes(pdf_file_bytes):
         assert pipeline is not None
     except RuntimeError as e:
         pytest.fail(f"Pipeline initialization failed with RuntimeError: {e}")
+
+
+def test_pipeline_with_bank():
+    file_path = Path("src/monopoly/examples/example_statement.pdf")
+    pipeline = Pipeline(file_path=file_path, bank=ExampleBank)
+    transactions = pipeline.statement.transactions
+    assert len(transactions) == 53
+    assert transactions[0].description == "LAST MONTH'S BALANCE"
+
+
+def test_pipeline_with_bad_bank():
+    file_path = Path("src/monopoly/examples/example_statement.pdf")
+    pipeline = Pipeline(file_path=file_path, bank=Dbs)
+    with pytest.raises(ValueError, match="Statement date not found"):
+        pipeline.extract()
 
 
 def test_pipeline_initialization_with_file_path():
