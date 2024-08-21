48a8de3d4b5aa061a142e65da7d9971159c79903
Benjamin Dornel <benjamindornel@gmail.com>
2024-08-22 00:20:07 +0800

    docs: add docstring for DateFormats

--- src/monopoly/constants/date.py
+++ src/monopoly/constants/date.py
@@ -10,6 +10,8 @@ from strenum import StrEnum
 # flake8: noqa
 # pylint: disable=line-too-long
 class DateFormats(StrEnum):
+    """Holds a case-insensitive list of common ISO 8601 date formats"""
+
     D = r"(?i:1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)"
     DD = r"(?i:01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)"
     M = r"(?i:1|2|3|4|5|6|7|8|9|10|11|12)"
