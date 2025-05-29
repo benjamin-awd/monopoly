b2c4cd547a0849342769e24a146f50cad456f0b9
Benjamin Dornel <benjamindornel@gmail.com>
2025-05-30 00:38:23 +0800

    chore(statement): allow custom date string using regex named groups

--- tests/unit/test_statement_date_multiline.py
+++ tests/unit/test_statement_date_multiline.py
@@ -1,16 +1,25 @@
-from re import compile as regex
-from monopoly.constants import ISO8601
-from monopoly.statements import BaseStatement
-from monopoly.config import MultilineConfig
 from datetime import datetime
+from re import compile as regex
+
+from monopoly.config import MultilineConfig
+from monopoly.statements import BaseStatement
+
 
 def test_statement_date_multiline(statement: BaseStatement):
     statement.pages[0].lines = [
-        "Statement cycle   23 Apr 2025 - 23 May",
-        "                                2025"
+        "   01-01                                                      23 Apr 2025 - 23 May",
+        "                                                                          Statement cycle",
+        "   02-123                                                                            2025",
     ]
 
-    statement.config.statement_date_pattern = regex(rf"\- {ISO8601.DD_MMM_YYYY}")
-    statement.config.multiline_config = MultilineConfig(multiline_statement_date=True, multiline_transactions=False)
-
+    statement.config.statement_date_pattern = regex(
+        r"-\s*"
+        r"(?P<day>\d{2})\s+"
+        r"(?P<month>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+"
+        r".*?"
+        r"(?P<year>20\d{2}\b)"
+    )
+    statement.config.multiline_config = MultilineConfig(
+        multiline_statement_date=True, multiline_transactions=False
+    )
     assert statement.statement_date == datetime(2025, 5, 23)
