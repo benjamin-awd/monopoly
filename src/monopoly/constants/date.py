48cbbb3424a108a8da29823fa71067ba323f00e9
Benjamin Dornel <benjamindornel@gmail.com>
2025-06-06 22:21:53 +0800

    chore: add period to date patterns

--- src/monopoly/constants/date.py
+++ src/monopoly/constants/date.py
@@ -24,15 +24,15 @@ class DateFormats(StrEnum):
 
 
 class ISO8601(RegexEnum):
-    DD_MM = rf"\b({DateFormats.DD}[\/\-\s]{DateFormats.MM})"
-    DD_MM_YY = rf"\b({DateFormats.DD}[\/\-\s]{DateFormats.MM}[\/\-\s]{DateFormats.YY})"
+    DD_MM = rf"\b({DateFormats.DD}[\/\-\s.]{DateFormats.MM})"
+    DD_MM_YY = rf"\b({DateFormats.DD}[\/\-\s.]{DateFormats.MM}[\/\-\s.]{DateFormats.YY})"
     DD_MM_YYYY = rf"\b({DateFormats.DD}[\/\-\s.]{DateFormats.MM}[\/\-\s.]{DateFormats.YYYY})"
-    DD_MMM = rf"\b({DateFormats.DD}[-\s]{DateFormats.MMM})"
-    DD_MMM_RELAXED = DD_MMM.replace(r"[-\s]", r"(?:[-\s]|)")
-    DD_MMM_YY = rf"\b({DateFormats.DD}[-\s]{DateFormats.MMM}[-\s]{DateFormats.YY})"
-    DD_MMM_YYYY = rf"\b({DateFormats.DD}[-\s]{DateFormats.MMM}[,\s]{{1,2}}{DateFormats.YYYY})"
-    MM_DD = rf"\b({DateFormats.MM}[\/\-\s]{DateFormats.DD})"
-    MM_DD_YY = rf"\b({DateFormats.MM}[\/\-\s]{DateFormats.DD}[\/\-\s]{DateFormats.YY})"
+    DD_MMM = rf"\b({DateFormats.DD}[\/\-\s.]{DateFormats.MMM})"
+    DD_MMM_RELAXED = DD_MMM.replace(r"[\/\-\s.]", r"(?:[\/\-\s.]|)")
+    DD_MMM_YY = rf"\b({DateFormats.DD}[\/\-\s.]{DateFormats.MMM}[\/\-\s.]{DateFormats.YY})"
+    DD_MMM_YYYY = rf"\b({DateFormats.DD}[\/\-\s.]{DateFormats.MMM}[,\s]{{1,2}}{DateFormats.YYYY})"
+    MM_DD = rf"\b({DateFormats.MM}[\/\-\s.]{DateFormats.DD})"
+    MM_DD_YY = rf"\b({DateFormats.MM}[\/\-\s.]{DateFormats.DD}[\/\-\s.]{DateFormats.YY})"
     MMMM_DD_YYYY = rf"\b({DateFormats.MMMM}\s{DateFormats.DD}[,\s]{{1,2}}{DateFormats.YYYY})"
-    MMM_DD = rf"\b({DateFormats.MMM}[-\s]{DateFormats.DD})"
-    MMM_DD_YYYY = rf"\b({DateFormats.MMM}[-\s]{DateFormats.DD}[,\s]{{1,2}}{DateFormats.YYYY})"
+    MMM_DD = rf"\b({DateFormats.MMM}[\/\-\s.]{DateFormats.DD})"
+    MMM_DD_YYYY = rf"\b({DateFormats.MMM}[\/\-\s.]{DateFormats.DD}[,\s]{{1,2}}{DateFormats.YYYY})"
