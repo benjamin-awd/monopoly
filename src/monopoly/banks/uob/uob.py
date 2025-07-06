a51ac7f396ee05cd034baee01025d2c8bdddea2a
Chua Song Ann <chuasongann@gmail.com>
2025-07-06 23:17:10 +0800

    Update uob.py

--- src/monopoly/banks/uob/uob.py
+++ src/monopoly/banks/uob/uob.py
@@ -6,6 +6,7 @@ from monopoly.config import MultilineConfig, StatementConfig
 from monopoly.constants import (
     ISO8601,
     BankNames,
+    CreditTransactionPatterns,
     DebitTransactionPatterns,
     EntryType,
     StatementBalancePatterns,
@@ -23,7 +24,7 @@ class Uob(BankBase):
         statement_date_pattern=regex(rf"Statement Date.*{ISO8601.DD_MMM_YYYY}"),
         header_pattern=regex(r"(Description of Transaction.*Transaction Amount)"),
         prev_balance_pattern=StatementBalancePatterns.UOB,
-        transaction_pattern=DebitTransactionPatterns.UOB,
+        transaction_pattern=CreditTransactionPatterns.UOB,
         multiline_config=MultilineConfig(multiline_descriptions=True),
         transaction_date_format="%d %b",
     )
